0749c460b1b13cc9b283bc728f19022db5bebcd4
Young Eun Kim <aakim0907@gmail.com>
2017-09-12 08:36:32 -0400

    work on discussion material

---
+++ 0914/0914_discussion.php
@@ -0,0 +1,64 @@
+<?php
+  // Define a variable "name" to store your the name "Alice" 
+  // and print "Hi Alice" using the variable and string concatenation
+  echo '1.';
+  // enter your code
+?>
+
+<?php
+  // Define a variable "birth_year" to store Alice's birth year, 2000.
+  // Use $name and $birth_year to print "Alice will be 20 years old in 2020"
+  echo '2.';
+  // enter your code
+?>
+
+<?php
+  // Use $birth_year to condition statement: 
+  // if Alice is old enough to drink, print "Alice may drink", 
+  // otherwise print "Alice may not drink"
+  echo '3.';
+  // enter your code
+?>
+
+<?php
+  // Rewrite exercise 4 in ternary
+  echo '4.';
+  // enter your code
+?>
+
+<?php
+  // Print the position of the letter 'i' in Alice's name
+  echo '5.';
+  // enter your code
+?>
+
+<?php
+  // If Alice's name is less than 4 letters, print "short"
+  // If Alice's name is longer than 7 letters, print "long"
+  // In other cases, print "medium"
+  echo '6.';
+  // enter your code
+?>
+
+<?php
+  // There are 10 apples in a basket and you eat one every day. 
+  // Until you have less than 3 apples, decrement the number of apples by one and print "Enough apples"
+  // If you have less than 3 apples, print "Need to buy more apples!"
+  // (use while loop)
+  echo '7.';
+  $basket = 10;
+  // enter your code
+?>
+
+<?php
+  // Print the numbers 20, 21, ..., 29 using "for" loop
+  echo '8.';
+  // enter your code
+?>
+
+<?php
+  // Looping from 1 to 10, print only the even numbers 
+  // (use continue)
+  echo '9.';
+  // enter your code
+?>
---
+++ 0914/index.php
@@ -0,0 +1,13 @@
+<html>
+  <body>
+    <h1>Hello from Dr. Chuck's HTML Page</h1>
+    <p>
+      <?php 
+        echo "Hi there.\n";
+        $answer = 6 * 7;
+        echo "The answer is $answer, what ";
+        echo "was the question again?\n";
+      ?>
+    </p>
+  </body>
+</html>
