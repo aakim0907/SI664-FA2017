567963de7f6ab9c99ee6cfd77c73204de9eb1917
Young Eun Kim <aakim0907@gmail.com>
2017-09-18 20:46:21 -0400

    work on week3 exercise

---
+++ 0921/0921_phpexercise.php
@@ -0,0 +1,59 @@
+<?php
+  echo "1. ";
+  // Define a function greet() that prints a message "Hello World"
+  // Print "Hello World" using greet()
+
+  // Write your code below
+?>
+
+<?php
+  echo "2. ";
+  // Define a function customized_greet() that takes a person's name as an argument, 
+  // and prints a personalized message, such as "Hello, Alice!"
+  // Print "Hello, {your name}"
+
+  // Write your code below
+  ?>
+
+<?php
+  echo "3. ";
+  // Define a function get_age() that take a person's birth year as an argument, 
+  // and prints messages, such as "Age: 21"
+  // Print your age
+  
+  // Write your code below
+?>
+
+<?php
+  echo "4. ";
+  // Define a function is_legal_to_drink() that takes a person's age as an argument, 
+  // and prints either "Legal" or "Illegal"
+  // Find out if it's legal for a person born in the year of 1997 to drink or not
+  
+  // Write your code below
+?>
+
+<?php
+  echo "5. ";
+  // Write a script to print the following lines
+  // Think of multiple ways you can write this code 
+
+  /*
+    Hello, Alice!
+    When you are 14, it is illegal for you to drink.
+    When you are 15, it is illegal for you to drink.
+    When you are 16, it is illegal for you to drink.
+    ...
+    When you are 20, it is illegal for you to drink.
+    When you are 21, it becomes legal for you to drink.
+    When you are 22, it is legal for you to drink.
+    ... 
+    When you are 25, it is legal for you to drink.
+  */
+
+  // Write your code below
+?>
+
+<?php
+  echo "\n";
+?>
